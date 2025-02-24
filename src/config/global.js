cd1c60b11a1dccb578772e1be2c90fd10bee9fa0
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2025-02-24 09:54:36 -0500

    Ajustes-cfa

--- src/config/global.js
+++ src/config/global.js
@@ -173,7 +173,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CF1_632223_DU.pdf',
+        download: 'downloads/CF1_632223_DU.zip',
       },
       {
         icono: 'fas fa-download',
