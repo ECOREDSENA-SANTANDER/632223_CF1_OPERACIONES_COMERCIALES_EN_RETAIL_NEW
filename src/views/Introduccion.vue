18146d3e7d8059618cccd1d8c2dbb68df650c1ae
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2025-04-21 12:29:39 -0500

    Ajuste

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -16,7 +16,7 @@
         figure(data-aos="fade-right").py-2
           .video.mb-1
             <iframe width="560" height="315" src="https://www.youtube.com/embed/2mWqJONbPrs?si=nLXCLAdqQUtM3z0E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-          figcaption Video. Manejo de inventarios en el #[em Retail]: introducción
+          figcaption Video. Manejo de inventarios en el #[em retail]: introducción
 </template>
 
 <script>
